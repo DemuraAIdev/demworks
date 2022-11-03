90726da47f0ac3b45486fd9c5a9b7ede4282ace1
DemuraAIdev <cyberaioff@gmail.com>
2022-11-03 19:46:01 +0700

    FIrst Upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  scope: '/app',
+  sw: 'service-worker.js',
+})
+
+const nextConfig = withPWA({
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
-}
+  distDir: 'build',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  }
+})
 
 module.exports = nextConfig
