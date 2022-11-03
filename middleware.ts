ebbe33ec266645a5a93cc9796ccb7f9868871987
DemuraAIdev <cyberaioff@gmail.com>
2022-11-03 20:45:37 +0700

    Mid Update

--- middleware.ts
+++ middleware.ts
@@ -20,7 +20,6 @@ export function middleware(request: NextRequest) {
 
     response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
     response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-    response.headers.set("X-Frame-Options", "DENY");
     response.headers.set("X-Content-Type-Options", "nosniff");
     response.headers.set("X-DNS-Prefetch-Control", "on");
     response.headers.set("Permissions-Policy", "geolocation=(self), microphone=()");
