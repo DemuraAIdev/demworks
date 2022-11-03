3513fbf8d9a3152adfb9bfa8428311fb83c8d660
DemuraAIdev <cyberaioff@gmail.com>
2022-11-03 20:37:00 +0700

    Response

--- middleware.ts
+++ middleware.ts
@@ -2,19 +2,27 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    // Clone the request headers and set a new header `x-version`
-    const requestHeaders = new Headers(request.headers);
-    requestHeaders.set('x-version', '13');
+    const response = NextResponse.next();
+    const csp = `
+    default-src 'self';
+    script-src 'self' *.twitter.com 'unsafe-eval' 'unsafe-inline' data:
+    child-src *.youtube.com *.google.com *.twitter.com https://cdpn.io https://codepen.io https://dbdiagram.io;
+    style-src 'self' *.googleapis.com 'unsafe-inline' 'unsafe-eval';
+    img-src 'self' data: https: blob: https://www.googletagmanager.com;
+    worker-src 'self' *.youtube.com *.google.com *.twitter.com;
+    connect-src *;
+    object-src 'none';
+    form-action 'self';
+    frame-ancestors 'none';
+    base-uri 'none';
+  `;
+    response.headers.set("Content-Security-Policy", csp.replace(/\n/g, ""));
 
-    // You can also set request headers in NextResponse.rewrite
-    const response = NextResponse.next({
-        request: {
-            // New request headers
-            headers: requestHeaders,
-        },
-    });
-
-    // Set a new response header `x-version`
-    response.headers.set('x-version', '13');
+    response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
+    response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+    response.headers.set("X-Frame-Options", "DENY");
+    response.headers.set("X-Content-Type-Options", "nosniff");
+    response.headers.set("X-DNS-Prefetch-Control", "on");
+    response.headers.set("Permissions-Policy", "geolocation=(self), microphone=()");
     return response;
 }
